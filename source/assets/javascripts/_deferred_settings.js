d47978b0b2634688cc7c48ba27d38d4856c82d8a
Jack Letourneau <github@jlet.org>
2015-07-20 12:32:38 -0400

    Set long (60s) timeout for web font loader.

--- source/assets/javascripts/_deferred_settings.js
+++ source/assets/javascripts/_deferred_settings.js
@@ -1,5 +1,8 @@
 window.WebFontConfig = {
   custom: {
     families: ['Merriweather:n4,n7']
-  }
+  },
+  // This can be set very high because the text content of the page is still
+  // visible before the web fonts load.
+  timeout: 60 * 1000
 };
